643a7cec458f4c9e60fe9df7abf61cf0b93b6895
Dan Rollo <danrollo@gmail.com>
2021-10-04 14:20:13 -0400

    update script for cncf csv file format and handle data problems in csv file

--- scripts/add-participants.py
+++ scripts/add-participants.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-
+import os
 import sys
 import csv
 import requests
 
-BASE_URL = "https://bug-bash.innovations-sandbox.sonatype.dev"
-# BASE_URL = "http://localhost:7777"
+#BASE_URL = "https://bug-bash.innovations-sandbox.sonatype.dev"
+BASE_URL = "http://localhost:7777"
 
 CAMPAIGN_NAME = "cncf-2021-10-11"
+
+# CSV_COLUMN_NAME_GITHUB_ID = "GitHub ID"
+CSV_COLUMN_NAME_GITHUB_ID = "Git Handle"
+CSV_COLUMN_NAME_DISPLAY_NAME = "Full Name"
 
 if len(sys.argv) != 2:
     print("Usage: ./participants.py CSV")
@@ -19,24 +23,40 @@ with open(sys.argv[1], newline='') as csvfile:
     rows = list(participants)
     count_added = 0
     count_skipped = 0
+    count_error = 0
+    exitCode = 0
     for participant in rows:
         print(f"participant: {participant}")
+
+        # detect email instead of required github ID
+        if "@" in participant[CSV_COLUMN_NAME_GITHUB_ID]:
+            print(f"*** invalid Github ID detected: participant: {participant}.*** \n*** Skipping participant.  ***")
+            count_error += 1
+            exitCode = 1
+            continue
+
+        # use suffix of github url if github ID is a urllib3
+        if "/" in participant[CSV_COLUMN_NAME_GITHUB_ID]:
+            participant[CSV_COLUMN_NAME_GITHUB_ID] = os.path.basename(participant[CSV_COLUMN_NAME_GITHUB_ID])
+
         send = {
             'campaignName': CAMPAIGN_NAME,
-            'GithubName': participant['GitHub ID'],
-            'email': participant['Email'],
-            'DisplayName': participant['GitHub ID']
+            'GithubName': participant[CSV_COLUMN_NAME_GITHUB_ID],
+            # 'email': participant['Email'],
+            'DisplayName': participant[CSV_COLUMN_NAME_DISPLAY_NAME]
         }
-        r = requests.request('GET', f"{BASE_URL}/participant/detail/{participant['GitHub ID']}")
+        r = requests.request('GET', f"{BASE_URL}/participant/detail/{participant[CSV_COLUMN_NAME_GITHUB_ID]}")
         if r.status_code == 200:
             count_skipped += 1
             print("Participant already exists, skipping")
         else:
             count_added += 1
-            print(f"Adding participant: {participant['GitHub ID']}")
+            print(f"Adding participant: {participant[CSV_COLUMN_NAME_GITHUB_ID]}")
             r = requests.request('PUT', f"{BASE_URL}/participant/add", json=send)
             print(r)
             print(r.text)
     print(f"done processing {len(rows)} participants")
     print(f"added {count_added}")
     print(f"skipped {count_skipped}")
+    print(f"errored {count_error}")
+    exit(exitCode)
