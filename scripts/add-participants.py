9285f0517f96d042904c72bd859853ee2b1012ab
Dan Rollo <danrollo@gmail.com>
2021-10-08 21:51:54 -0400

    make participant loader script verify github ID is valid

--- scripts/add-participants.py
+++ scripts/add-participants.py
@@ -5,7 +5,7 @@ import csv
 import requests
 
 BASE_URL = "https://bug-bash.innovations-sandbox.sonatype.dev"
-#BASE_URL = "http://localhost:7777"
+# BASE_URL = "http://localhost:7777"
 
 # CAMPAIGN_NAME = "cncf"
 CAMPAIGN_NAME = "cncf-2021-10-11"
@@ -13,6 +13,26 @@ CAMPAIGN_NAME = "cncf-2021-10-11"
 # CSV_COLUMN_NAME_GITHUB_ID = "GitHub ID"
 CSV_COLUMN_NAME_GITHUB_ID = "Git Handle"
 CSV_COLUMN_NAME_DISPLAY_NAME = "Full Name"
+
+default_gh_username = 'set_your_github_username_env'
+username = os.getenv('GITHUB_USERNAME', default_gh_username)
+token = os.getenv('GITHUB_TOKEN', '...')
+# create a re-usable session object with the user creds in-built
+gh_session = requests.Session()
+gh_session.auth = (username, token)
+
+
+def get_github_user(user):
+    url = f"https://api.github.com/users/{user}"
+    res = gh_session.get(url.format(user))
+    if res.status_code == 200:
+        return res.json()
+    else:
+        print(f"\n*** invalid github user: {user}. status code: {r.status_code} ***")
+        if r.status_code == 500 and username == default_gh_username:
+            print(f"\n*** did you forget to set the GITHUB_USERNAME environment variable? ***\n")
+        return
+
 
 if len(sys.argv) != 2:
     print("Usage: ./participants.py CSV")
@@ -26,12 +46,14 @@ with open(sys.argv[1], newline='') as csvfile:
     count_skipped = 0
     count_error = 0
     exitCode = 0
+    summary_error_message = []
     for participant in rows:
-        print(f"participant: {participant}")
 
         # detect email instead of required github ID
         if "@" in participant[CSV_COLUMN_NAME_GITHUB_ID]:
-            print(f"*** invalid Github ID detected: participant: {participant}.*** \n*** Skipping participant.  ***")
+            msg = f"\n*** invalid Github ID detected. Skipping participant. participant: {participant}. ***\n"
+            summary_error_message.append(msg)
+            print(msg)
             count_error += 1
             exitCode = 1
             continue
@@ -52,18 +74,33 @@ with open(sys.argv[1], newline='') as csvfile:
         r = requests.request('GET', f"{BASE_URL}/participant/detail/{participant[CSV_COLUMN_NAME_GITHUB_ID]}")
         if r.status_code == 200:
             count_skipped += 1
-            print("Participant already exists, skipping")
+            print(f"Participant already exists, skipping. participant: {participant}")
         else:
-            print(f"Adding participant: {participant[CSV_COLUMN_NAME_GITHUB_ID]}")
+            gh_user = get_github_user(participant[CSV_COLUMN_NAME_GITHUB_ID])
+            if gh_user is None:
+                if username == default_gh_username:
+                    msg = f"\n*** did you forget to set the GITHUB_USERNAME environment variable? ***"
+                    summary_error_message.append(msg)
+                msg = f"\n*** non-existent github id, error. participant: {participant} ***\n"
+                summary_error_message.append(msg)
+                count_error += 1
+                exitCode = 1
+                continue
+
+            print(f"Adding participant: {participant[CSV_COLUMN_NAME_GITHUB_ID]} participant: {participant}")
             r = requests.request('PUT', f"{BASE_URL}/participant/add", json=send)
             print(r)
             print(r.text)
             if r.status_code != 200 and r.status_code != 201:
+                msg = f"\n*** error adding participant. status code: {r.status_code}, participant: {participant} ***\n"
+                summary_error_message.append(msg)
                 count_error += 1
             else:
                 count_added += 1
 
     print(f"done processing {len(rows)} participants")
+    msg_summary = "\n".join(summary_error_message)
+    print(f"errors: {msg_summary}")
     print(f"added {count_added}")
     print(f"skipped {count_skipped}")
     print(f"errored {count_error}")
