c0bf15676ab6968f0ea6a9b642169b308ebfed3c
Dan Rollo <danrollo@gmail.com>
2021-11-03 14:29:27 -0400

    add db table constraints to check for empty values in names

--- db/migrations/v2/0001_create_db.up.sql
+++ db/migrations/v2/0001_create_db.up.sql
@@ -5,7 +5,7 @@ BEGIN;
 -- table: campaign
 CREATE TABLE campaign(
     ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name varchar(250) NOT NULL UNIQUE,
+    name varchar(250) NOT NULL UNIQUE CHECK (name <> ''),
     created_on timestamp NOT NULL DEFAULT NOW(),
     create_order SERIAL,
     start_on timestamp NOT NULL DEFAULT NOW(),
@@ -18,7 +18,7 @@ CREATE TABLE campaign(
 -- table: source_control_provider
 CREATE TABLE source_control_provider(
     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL CHECK (name <> ''),
     url TEXT UNIQUE NOT NULL
 );
 -- add scp providers
@@ -31,7 +31,7 @@ INSERT INTO source_control_provider (name,url) VALUES ('Bitbucket','https://bitb
 CREATE TABLE organization (
     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     fk_scp UUID references source_control_provider (Id) NOT NULL,
-    Organization TEXT NOT NULL,
+    Organization TEXT NOT NULL CHECK (Organization <> ''),
     unique (fk_scp, Organization)
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE organization (
 CREATE TABLE team(
     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     fk_campaign UUID references campaign (Id) NOT NULL,
-    name varchar(250) NOT NULL,
+    name varchar(250) NOT NULL CHECK (name <> ''),
     unique (fk_campaign, name)
 );
 
@@ -49,7 +49,7 @@ CREATE TABLE team(
 CREATE TABLE bug(
      ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
      fk_campaign UUID references campaign (Id) NOT NULL,
-     category varchar(255) NOT NULL,
+     category varchar(255) NOT NULL CHECK (category <> ''),
      pointValue int NOT NULL,
      unique (fk_campaign, category)
 );
@@ -60,7 +60,7 @@ CREATE TABLE participant(
     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     fk_campaign UUID references campaign (Id) NOT NULL,
     fk_scp UUID references source_control_provider (Id) NOT NULL,
-    login_name varchar(250) NOT NULL,
+    login_name varchar(250) NOT NULL CHECK (login_name <> ''),
     Email varchar(250),
     DisplayName varchar(250),
     Score int,
@@ -75,8 +75,8 @@ CREATE TABLE participant(
 CREATE TABLE scoring_event (
     fk_campaign UUID references campaign (Id) NOT NULL,
     fk_scp UUID references source_control_provider (Id),
-    repoOwner TEXT NOT NULL,
-    repoName TEXT NOT NULL,
+    repoOwner TEXT NOT NULL CHECK (repoOwner <> ''),
+    repoName TEXT NOT NULL CHECK (repoName <> ''),
     pr INT,
     username TEXT NOT NULL,
     points INT NOT NULL,
