91b94c2755cd5cbce2c51df2486affca0f234f78
Dan Rollo <danrollo@gmail.com>
2022-02-25 15:33:18 -0500

    use app version

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
         <NxPageHeader className={styles["nx-page-header"]}
             productInfo={{
                 name: (process.env.REACT_APP_BUG_BASH_APP_NAME) ? process.env.REACT_APP_BUG_BASH_APP_NAME : packageJson.name,
-                version: (process.env.REACT_APP_BUG_BASH_VERSION) ? process.env.REACT_APP_BUG_BASH_VERSION : packageJson.version
+                version: packageJson.version
             }}
         />
     )
